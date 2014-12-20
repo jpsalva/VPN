573709d292aa4c36e5bad147e6f81b9eae401d74
jpsalva <jpsalva@JPs-MacBook-Pro.local>
2014-12-20 06:32:07 -0500

    VPN

---
+++ setup.sh
@@ -0,0 +1,268 @@
+#!/bin/bash
+# Interactive PoPToP install script for an OpenVZ VPS
+# Tested on Debian 5, 6, and Ubuntu 11.04
+# April 2, 2013 v1.11
+# http://www.putdispenserhere.com/pptp-debian-ubuntu-openvz-setup-script/
+
+dpkg --configure -a
+
+echo "######################################################"
+echo "Interactive PoPToP Install Script for an OpenVZ VPS"
+echo
+echo "Make sure to contact your provider and have them enable"
+echo "IPtables and ppp modules prior to setting up PoPToP."
+echo "PPP can also be enabled from SolusVM."
+echo
+echo "You need to set up the server before creating more users."
+echo "A separate user is required per connection or machine."
+echo "######################################################"
+echo
+echo
+echo "######################################################"
+echo "Select on option:"
+echo "1) Set up new PoPToP server AND create one user"
+echo "2) Create additional users"
+echo "######################################################"
+read x
+if test $x -eq 1; then
+	echo "Enter username that you want to create (eg. client1 or john):"
+	read u
+	echo "Specify password that you want the server to use:"
+	read p
+
+# get the VPS IP
+ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+
+echo
+echo "######################################################"
+echo "Downloading and Installing PoPToP"
+echo "######################################################"
+apt-get update
+apt-get -y install pptpd
+
+echo
+echo "######################################################"
+echo "Creating Server Config"
+echo "######################################################"
+cat > /etc/ppp/pptpd-options <<END
+name pptpd
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe-128
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
+proxyarp
+nodefaultroute
+lock
+nobsdcomp
+END
+
+# setting up pptpd.conf
+echo "option /etc/ppp/pptpd-options" > /etc/pptpd.conf
+echo "logwtmp" >> /etc/pptpd.conf
+echo "localip $ip" >> /etc/pptpd.conf
+echo "remoteip 10.1.0.1-100" >> /etc/pptpd.conf
+
+# adding new user
+echo "$u	*	$p	*" >> /etc/ppp/chap-secrets
+
+echo
+echo "######################################################"
+echo "Forwarding IPv4 and Enabling it on boot"
+echo "######################################################"
+cat >> /etc/sysctl.conf <<END
+net.ipv4.ip_forward=1
+END
+sysctl -p
+
+echo
+echo "######################################################"
+echo "Updating IPtables Routing and Enabling it on boot"
+echo "######################################################"
+iptables -t nat -A POSTROUTING -j SNAT --to $ip
+# saves iptables routing rules and enables them on-boot
+iptables-save > /etc/iptables.conf
+
+cat > /etc/network/if-pre-up.d/iptables <<END
+#!/bin/sh
+iptables-restore < /etc/iptables.conf
+END
+
+chmod +x /etc/network/if-pre-up.d/iptables
+cat >> /etc/ppp/ip-up <<END
+ifconfig ppp0 mtu 1400
+END
+
+echo
+echo "######################################################"
+echo "Restarting PoPToP"
+echo "######################################################"
+sleep 5
+/etc/init.d/pptpd restart
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
+###################################################################################################
+###################################################################################################
+###################################################################################################
+
+
+echo "Y" | apt-get install transmission-daemon
+
+echo "Y" | apt-get install openjdk-6-jre
+
+wget http://aarnet.dl.sourceforge.net/project/subsonic/subsonic/4.9/subsonic-4.9.deb
+
+dpkg -i subsonic-4.9.deb
+
+cat > /var/lib/transmission-daemon/info/settings.json <<END
+{
+    "alt-speed-down": 50,
+    "alt-speed-enabled": false,
+    "alt-speed-time-begin": 540,
+    "alt-speed-time-day": 127,
+    "alt-speed-time-enabled": false,
+    "alt-speed-time-end": 1020,
+    "alt-speed-up": 50,
+    "bind-address-ipv4": "0.0.0.0",
+    "bind-address-ipv6": "::",
+    "blocklist-enabled": false,
+    "blocklist-url": "http://www.example.com/blocklist",
+    "cache-size-mb": 4,
+    "dht-enabled": true,
+    "download-dir": "/var/lib/transmission-daemon/downloads",
+    "download-limit": 100,
+    "download-limit-enabled": 0,
+    "download-queue-enabled": true,
+    "download-queue-size": 5,
+    "encryption": 1,
+    "idle-seeding-limit": 30,
+    "idle-seeding-limit-enabled": false,
+    "incomplete-dir": "/root/Downloads",
+    "incomplete-dir-enabled": false,
+    "lpd-enabled": false,
+    "max-peers-global": 200,
+    "message-level": 2,
+    "peer-congestion-algorithm": "",
+    "peer-limit-global": 240,
+    "peer-limit-per-torrent": 60,
+    "peer-port": 51413,
+    "peer-port-random-high": 65535,
+    "peer-port-random-low": 49152,
+    "peer-port-random-on-start": false,
+    "peer-socket-tos": "default",
+    "pex-enabled": true,
+    "port-forwarding-enabled": false,
+    "preallocation": 1,
+    "prefetch-enabled": 1,
+    "queue-stalled-enabled": true,
+    "queue-stalled-minutes": 30,
+    "ratio-limit": 2,
+    "ratio-limit-enabled": false,
+    "rename-partial-files": true,
+    "rpc-authentication-required": false,
+    "rpc-bind-address": "0.0.0.0",
+    "rpc-enabled": true,
+    "rpc-password": "torrents",
+    "rpc-port": 9091,
+    "rpc-url": "/transmission/",
+    "rpc-username": "torrents",
+    "rpc-whitelist": "127.0.0.1",
+    "rpc-whitelist-enabled": false,
+    "scrape-paused-torrents-enabled": true,
+    "script-torrent-done-enabled": false,
+    "script-torrent-done-filename": "",
+    "seed-queue-enabled": false,
+    "seed-queue-size": 10,
+    "speed-limit-down": 100,
+    "speed-limit-down-enabled": false,
+    "speed-limit-up": 100,
+    "speed-limit-up-enabled": false,
+    "start-added-torrents": true,
+    "trash-original-torrent-files": false,
+    "umask": 18,
+    "upload-limit": 100,
+    "upload-limit-enabled": 0,
+    "upload-slots-per-torrent": 14,
+    "utp-enabled": true
+}
+END
+
+service transmission-daemon reload
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
+###################################################################################################
+###################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo
+echo "######################################################"
+echo "Server setup complete!"
+echo "Connect to your VPS at $ip with these credentials:"
+echo "Username:$u ##### Password: $p"
+echo "######################################################"
+
+# runs this if option 2 is selected
+elif test $x -eq 2; then
+	echo "Enter username that you want to create (eg. client1 or john):"
+	read u
+	echo "Specify password that you want the server to use:"
+	read p
+
+# get the VPS IP
+ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+
+# adding new user
+echo "$u	*	$p	*" >> /etc/ppp/chap-secrets
+
+echo
+echo "######################################################"
+echo "Addtional user added!"
+echo "Connect to your VPS at $ip with these credentials:"
+echo "Username:$u ##### Password: $p"
+echo "######################################################"
+
+
+
+else
+echo "Invalid selection, quitting."
+exit
+fi
